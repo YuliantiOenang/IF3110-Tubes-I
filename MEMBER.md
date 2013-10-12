b27e0a2dceed6797e2c79c6ebdca7c6839bbd637
Ahmad Fauzan <azon04@gmail.com>
2013-10-12 17:01:48 +0700

    Header HTML+CSS

---
+++ MEMBER.md
@@ -0,0 +1,2 @@
+NIM 		NAMA 			MAIL 				GITHUB_ID
+13510004	Ahmad Fauzan	azon04@gmail.com	azon04
---
+++ src/index.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		<title>RuSerBa</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+	</head>
+	<body>
+		<div id="header">
+			<div id="navigation">
+			<div id="logo">
+				<img src="img/logo.png" width="100px"/>
+			</div>
+			<div id="navCategory">
+				<a href="#" class="header_item">Makanan</a>
+				<a href="#" class="header_item">Minuman</a>
+				<a href="#" class="header_item">Alat Tulis</a>
+				<a href="#" class="header_item">Kebersihan</a>
+				<a href="#" class="header_item">Obat-obatan</a>
+			</div>
+			
+			<div id="navMember">
+				<a href="#" class="button">Login</a>
+				<a href="#" class="button">Register</a>
+				<!--<a href="#" class="button">Logout</a> -->
+				<a href="#" class="button">Shopping Bag</a> 
+			</div>
+			<form id="search">
+				<input type="text" class="text_input" />
+				<input type="submit" class="button" value="search"></input>
+			</form>
+			</div>
+		</div>
+	</body>
+</html>

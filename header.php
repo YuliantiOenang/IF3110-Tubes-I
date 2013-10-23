a2c1d19b6a6ab88a8587fe86ddf0a8724d8d4fa2
Rifki Afina Putri <rifkiafinaputri@gmail.com>
2013-10-23 21:26:58 +0700

    Layout Home

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>RuSerBa</title>
+ 
+<link rel="stylesheet" href="css/main.css" type="text/css" /> 
+
+</head>
+ 
+<body id="index" class="home">
+
+<div style="width:1000px; margin-left:auto; margin-right:auto">
+
+<header id="banner" class="body">
+	
+	<span style="float:left"><a href="#"><img src="images/logo.png" alt="RuSerBa Logo" width="110" height="110"/></a></span>
+	<h1><span><a href="#">RuSerBa<br><strong>Ruko Serba Ada</strong></a></span></h1>
+ 
+	<nav><ul>
+		<li class="active"><a href="#">Home</a></li>
+		<li><a href="#">Kategori Barang</a></li>
+		<li><a href="#">Login</a></li>
+		<li><a href="#">Daftar</a></li>
+		
+		<li style="float:right"><button type="button">Search</button></li>
+		<li style="float:right"><input type="text" name="search" placeholder="Cari Barang"></li>
+		
+	</ul></nav>
+ 
+</header><!-- /#banner -->
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include "header.php";?>
+<article id="featured" class="body">
+		<h2>Featured Product</h2>
+		<h3><a href="#">Kategori Makanan</a></h3>
+		<p>Dapatkan daging segar dengan diskon 10%!</p>
+	
+</article><!-- /#featured -->
+
+<footer id="contentinfo" class="body">
+	<address id="about" class="vcard body">
+		<img src="images/logo.png" alt="RuSerBa Logo" class="photo" width="40" height="40"/>
+		
+		<span class="primary">
+			<strong><a href="#" class="fn url">RuSerBa</a></strong>
+ 
+			<span class="role">Ruko Serba Ada</span>
+		</span><!-- /.primary -->
+		
+		<span class="bio">RuSerBa adalah website yang menjual barang-barang kebutuhan sehari-hari secara online.</span>
+ 
+	</address><!-- /#about -->
+	
+	<p>Copyright 2013 - Kelompok Terserah Kelas K2</p>
+	
+</footer><!-- /#contentinfo -->
+
+</div>
+</body>
+</html>

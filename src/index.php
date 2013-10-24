27db4344288598b72ca3b2d1029d9a7e51f235c4
Faiz Ilham <indigo.republica@gmail.com>
2013-10-25 06:24:28 +0700

    coba localStorage

--- src/index.php
+++ src/index.php
@@ -13,7 +13,7 @@
 <!DOCTYPE html />
 <html>
 <head>
-<script src="script.js"></script>
+<script src="js/ajax.js"></script>
 <script>
 	function send(data){
 		sendAjax({"action":"add", "id_barang":0, "jumlah":1}, "transaksi.php", callback_send);
@@ -22,10 +22,22 @@
 	function callback_send(response){
 		alert(JSON.stringify(response));
 	}
+	
+	function test(){
+		if (localStorage.getItem("test") === null){
+			localStorage.test = 0;
+			document.write(0);
+		}else if(localStorage.test < 5){
+			localStorage.test++;
+			document.write(localStorage.test);
+		}else{
+			localStorage.removeItem("test");
+			document.write("resetted");
+		}
+	}
 </script>
 </head>
 
-<body>
-<input type="button" onclick="send('a')" value="button"/>
+<body onload="test()">
 </body>
 </html>

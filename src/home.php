0d82b72e85a42342c635f519c587eb48dfa63760
nashr <muhammad.nassirudin@gmail.com>
2013-10-25 18:52:49 +0700

    Layouting header

--- src/home.php
+++ src/home.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <link rel="stylesheet" type="text/css" href="header.css">
+</head>
 <body>
 
-<h1>HOME</h1><br/>
+<h1 class="center">HOME</h1><br/>
 
 <img src="logo.png"></img><br/>
 <img src="logo.png"></img><br/>
 <img src="logo.png"></img><br/>
+
 <?php
 //echo "HOME";
 ?>
--- src/index.php
+++ src/index.php
@@ -1,19 +1,52 @@
+<!-- HOME -->
+
 <!DOCTYPE html>
 <html>
+<title>Beranda Toko</title>
+<head>
+    <link rel="stylesheet" type="text/css" href="header.css">
+	<meta charset="UTF-8">
+</head>
 <body>
-
-<h1>Ruserba "LapAn"</h1>
-<a href="home.php"><img src="logo.png"></img></a><br/>
-<a href="kategori1.php">Kategori 1</a> <br/> 
-<a href="login.php">Login</a> <br/>
-<a href="daftar.php">Daftar</a> <br/>
-
-<form action="search.php" method="post">
-<input type="text" name="name">
-<input type="submit" value="search"><br/>
-
-<a href="shoppingbag.php">Shopping Bag</a> <br/>
-
-</form>
+<div id="header">
+	<div id="logo">
+		<a href="index.php"><img src='img/logo.png'></img></a>
+	</div>
+	<div id="title">
+		<h1 id="maintitle">RuSerBa LapAn</h1>
+		<h1 id="subtitle">Ruko Serba Ada Lapak Andalan</h1>
+	</div>
+	<div id="user">
+		<ul id="nav_bar">
+			<li id="nav_bar_hor"><a id="nav" href="#">SHOPPING BAG</a></li>
+			<li id="nav_bar_hor"><a id="nav" href="#">MASUK</a></li>
+			<li id="nav_bar_hor">atau</li>
+			<li id="nav_bar_hor"><a id="nav" href="#">DAFTAR</a></li>
+		</ul>
+	</div>
+	<div id="navigation">
+		<ul id="nav_bar">
+			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI1</a></li>
+			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI2</a></li>
+			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI3</a></li>
+			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI4</a></li>
+			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI5</a></li>
+		</ul>
+	</div>
+	<div id="search">
+		<form action="search.php" method="post">
+			<input type="text" name="name">
+			<input type="submit" value="CARI">
+		</form>
+	</div>
+</div>
+<div id="sider_left"></div>
+<div id="content">
+	<!--<a href="shoppingbag.php">Shopping Bag</a>
+	<a href="kategori1.php">Kategori 1</a>
+	<a href="login.php">Login</a>
+	<a href="daftar.php">Daftar</a>-->
+</div>
+<div id="sider_right"></div>
 </body>
 </html> 

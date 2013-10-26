b4a4cad7912337f863b8ac9d915f196bcd015d74
nashr <muhammad.nassirudin@gmail.com>
2013-10-26 12:22:49 +0700

    pergantian layout dasar

--- src/index.php
+++ src/index.php
@@ -1,52 +1,45 @@
 <!-- HOME -->
-
 <!DOCTYPE html>
 <html>
-<title>Beranda Toko</title>
-<head>
-    <link rel="stylesheet" type="text/css" href="header.css">
-	<meta charset="UTF-8">
-</head>
-<body>
-<div id="header">
-	<div id="logo">
-		<a href="index.php"><img src='img/logo.png'></img></a>
-	</div>
-	<div id="title">
-		<h1 id="maintitle">RuSerBa LapAn</h1>
-		<h1 id="subtitle">Ruko Serba Ada Lapak Andalan</h1>
-	</div>
-	<div id="user">
-		<ul id="nav_bar">
-			<li id="nav_bar_hor"><a id="nav" href="#">SHOPPING BAG</a></li>
-			<li id="nav_bar_hor"><a id="nav" href="#">MASUK</a></li>
-			<li id="nav_bar_hor">atau</li>
-			<li id="nav_bar_hor"><a id="nav" href="#">DAFTAR</a></li>
-		</ul>
-	</div>
-	<div id="navigation">
-		<ul id="nav_bar">
-			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI1</a></li>
-			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI2</a></li>
-			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI3</a></li>
-			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI4</a></li>
-			<li id="nav_bar_hor"><a id="nav_same_width" href="#">KATEGORI5</a></li>
-		</ul>
-	</div>
-	<div id="search">
-		<form action="search.php" method="post">
-			<input type="text" name="name">
-			<input type="submit" value="CARI">
-		</form>
-	</div>
-</div>
-<div id="sider_left"></div>
-<div id="content">
-	<!--<a href="shoppingbag.php">Shopping Bag</a>
-	<a href="kategori1.php">Kategori 1</a>
-	<a href="login.php">Login</a>
-	<a href="daftar.php">Daftar</a>-->
-</div>
-<div id="sider_right"></div>
-</body>
-</html> 
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/general.css"></link>
+		<meta charset="UTF-8"></meta>
+		<title>
+			Beranda Toko
+		</title>
+	</head>
+	<body>
+		<div id="header">
+			<div id="logo">LOGO</div>
+			<div id="header_top">
+				<ul id="nav_bar_right">
+					<li id="nav_hor"><a href="#">SHOPPING BAG</a></li>
+					<li id="nav_hor">|</li>
+					<li id="nav_hor"><a href="#">MASUK</a></li>
+					<li id="nav_hor">|</li>
+					<li id="nav_hor"><a href="#">DAFTAR</a></li>
+				</ul>
+			</div>
+			<div id="header_bottom">
+				<ul id="nav_bar">
+					<li id="nav_hor_wrap"><a href="#">KATEGORI 1</a></li>
+					<li id="nav_hor_wrap"><a href="#">KATEGORI 2</a></li>
+					<li id="nav_hor_wrap"><a href="#">KATEGORI 3</a></li>
+					<li id="nav_hor_wrap"><a href="#">KATEGORI 4</a></li>
+					<li id="nav_hor_wrap"><a href="#">KATEGORI 5</a></li>
+				</ul>
+				<form id="search" action="#" method="post">
+					<input id="text_field" type="text" name="name"></input>
+					<input id="button" type="submit" value="CARI"></input>
+				</form>
+			</div>
+		</div>
+		<div id="content">
+			content goes here
+		</div>
+		<div id="footer">
+			<p>Copyright 2013 by <b>Kelompok LapAn</b></p>
+			<p>Muhammad Nassirudin - 13511044 | Arief Pradana - 13511062 | Ryan Ignatius Hadiwijaya - 13511070</p>
+		</div>
+	</body>
+</html>

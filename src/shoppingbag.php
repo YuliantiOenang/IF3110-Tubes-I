4b463d84eca2456e8031e411f0232caac0b9a5cb
nashr <muhammad.nassirudin@gmail.com>
2013-10-26 23:58:11 +0700

    Final layout kecuali content sekalian ngerapiin codingan(?)

--- src/shoppingbag.php
+++ src/shoppingbag.php
@@ -10,28 +10,45 @@
 	</head>
 	<body>
 		<div id="header">
-			<div id="logo">LOGO</div>
 			<div id="header_top">
 				<ul id="nav_bar_right">
-					<li id="nav_hor"><a href="#">SHOPPING BAG</a></li>
+					<li id="nav_hor">
+						<a href="shoppingbag.php">
+							<img src="img/cart.png"></img>
+						</a>
+					</li>
 					<li id="nav_hor">|</li>
-					<li id="nav_hor"><a href="#">MASUK</a></li>
+					<li id="nav_hor"><a href="#"><b>MASUK</b></a></li>
 					<li id="nav_hor">|</li>
-					<li id="nav_hor"><a href="#">DAFTAR</a></li>
+					<li id="nav_hor"><a href="register.php"><b>DAFTAR</b></a></li>
 				</ul>
 			</div>
 			<div id="header_bottom">
 				<ul id="nav_bar">
-					<li id="nav_hor_wrap"><a href="#">KATEGORI 1</a></li>
-					<li id="nav_hor_wrap"><a href="#">KATEGORI 2</a></li>
-					<li id="nav_hor_wrap"><a href="#">KATEGORI 3</a></li>
-					<li id="nav_hor_wrap"><a href="#">KATEGORI 4</a></li>
-					<li id="nav_hor_wrap"><a href="#">KATEGORI 5</a></li>
+					<li id="nav_hor_wrap"></li>
+					<li id="nav_hor_wrap"><a href="category.php">KATEGORI 1</a></li>
+					<li id="nav_hor_wrap"><a href="category.php">KATEGORI 2</a></li>
+					<li id="nav_hor_wrap"><a href="category.php">KATEGORI 3</a></li>
+					<li id="nav_hor_wrap"><a href="category.php">KATEGORI 4</a></li>
+					<li id="nav_hor_wrap"><a href="category.php">KATEGORI 5</a></li>
+					<li id="search">
+						<form action="#" method="post">
+							<input id="text_field" type="text" name="name"></input>
+							<input id="button" type="submit" value="CARI"></input>
+						</form>
+					</li>
 				</ul>
-				<form id="search" action="#" method="post">
-					<input id="text_field" type="text" name="name"></input>
-					<input id="button" type="submit" value="CARI"></input>
-				</form>
+			</div>
+			<a id="logo" href="index.php">
+				<img src="img/logo.png"></img>
+			</a>
+			<div id="title">
+				<h1>
+					RuSerBa LapAn
+				</h1>
+				<p id="subtitle">
+					Ruko Serba Ada Lapak Andalan
+				</p>
 			</div>
 		</div>
 		<div id="content">

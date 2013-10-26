1310e968009fc010f0be1b8c8fbffa141e12670f
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2013-10-26 07:21:28 +0700

    Add error reporting and init

--- src/app/config.php
+++ src/app/config.php
@@ -1,5 +1,14 @@
 <?php
-	/**
-	 * File untuk penanganan konfigurasi, misalnya database
-	 *
-	 */
+/**
+ * File untuk penanganan konfigurasi
+ * Misanya environment, database
+ */
+
+
+/**
+ * Value yang diperbolehkan:
+ *		development
+ *		testing
+ *		production
+ **/
+define('ENVIRONMENT', 'development');
---
+++ src/framework/init.php
@@ -0,0 +1,18 @@
+<?php
+
+require SITEPATH . '/framework/' . 'base_controller.class.php';
+require SITEPATH . '/framework/' . 'registry.class.php';
+require SITEPATH . '/framework/' . 'router.class.php';
+require SITEPATH . '/framework/' . 'template.class.php';
+
+//autoload class model
+function __autoload($class_name) {
+    $filename = strtolower($class_name) . '.class.php';
+    $file = SITEPATH . '/app/model/' . $filename;
+ 	require ($file);
+}
+
+//create new registry
+$registry = new Registry;
+//create database registry
+$registry->db = db::getInstance();

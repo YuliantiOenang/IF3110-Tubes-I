f2e70d615d2eeb9857583547108b555f420c5103
okaswara-p <okaswara@students.itb.ac.id>
2013-10-28 16:13:26 +0700

    Changing format from XHTML 1.0 Strict into HTML5.

--- src/begin.php
+++ src/begin.php
@@ -5,10 +5,10 @@
 	if (!isset($js_file)) $js_file = array();
 	else if (!is_array($js_file)) $js_file = [ $js_file ];
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+	<meta charset="UTF-8" />
 	<title><?php echo $page_title; ?></title>
 	<link rel="stylesheet" type="text/css" href="styles/global.css" />
 	<script type="text/javascript" src="scripts/global.js"></script>
@@ -24,4 +24,5 @@
 	?>
 </head>
 <body>
+	<div id="popuplayer"></div>
 	<?php include_once('core_begin.php'); ?>

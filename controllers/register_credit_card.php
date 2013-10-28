75a1021cfb00750cc6f8128d169fbc6a995433bd
Genta Indra Winata <gentaindrawinata@gmail.com>
2013-10-28 10:18:05 +0700

    update template database

--- controllers/register_credit_card.php
+++ controllers/register_credit_card.php
@@ -1,21 +1,6 @@
 <?php
 	// Create connection
-	$con=mysqli_connect("127.0.0.1","root","","toko_imba");
-
-	// Check connection
-	if (mysqli_connect_errno($con)){
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-
-	//mysqli_query($con,"INSERT INTO pengguna (id_pengguna, nama_pengguna)
-	//VALUES ('Peter', 'Griffin')");
-
-	//check data posted
-	//echo "Submit query".'<br/>';
-	//echo $_POST['name'].'<br/>';
-	//echo $_POST['username'].'<br/>';
-	//echo $_POST['password'].'<br/>';
-	//echo $_POST['email'].'<br/>';
+	include ("connect_database.php");
 	
 	$card_number = $_POST['card_number'];
 	$name = $_POST['name'];
--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,11 @@
 		<img src="../img/logo.png" width=200px></img>
 	</div>
 	<div class = "login">
-		<a href="">register</a> or
-		<a href="">login</a>
+		<a href="../pages/register_user.php">register</a> or
+		<a href="../pages/login_user.php">login</a>
+	</div>
+	<div class = "user">
+		<br/>
+		<a href="../pages/profile_user.php">view/edit profile</a>
 	</div>
 </div>

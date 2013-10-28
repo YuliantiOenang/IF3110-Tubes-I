c3d431990f67062bb47fbc9d0a4d220196ab3b04
Salvian Reynaldi <salvianreynaldi@gmail.com>
2013-10-28 13:42:04 +0700

    initial push

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>
+<html>
+		<?PHP include 'macro/header.php'?>
+	<link rel="stylesheet" type="text/css" href="css/homePage.css">
+	<body>
+		<div id="homeContent">	
+		<ul id="itemPic">
+			<li>
+				<img src="images/logoMak.png" alt="Makanan">
+				<div class="itemInf">
+					<h2>Makanan</h2>
+					<p>Nikmati makanan<br>dengan kualitas premium</p>
+					<a href="#" title="Makanan">Cah Sapi + Nasi</a>
+					<a href="#" title="Pakaian">Campuran Buah</a>
+					<a href="#" title="Pakaian">Ayam Katsu <i>Oeh</i></a>
+				</div>
+			</li>
+			<li>
+				<img src="images/logoMin.png" alt="Minuman">
+				<div class="itemInf">
+					<h2>Minuman</h2>
+					<p>Kesegarannya abadi<br>tuk hilangkan dahaga</p>
+					<a href="#" title="Minuman">Kopi Warna-Warni</a>
+					<a href="#" title="Pakaian">Darah Cinta</a>
+					<a href="#" title="Pakaian">Jus KejuTan</a>
+				</div>
+			</li>
+			<li>
+				<img src="images/logoPak.png" alt="Pakaian">
+				<div class="itemInf">
+					<h2>Pakaian</h2>
+					<p>Edisi terbatas berarti<br>anda-lah pusat perhatian</p>
+					<a href="#" title="Minuman">Formal Salvian</a>
+					<a href="#" title="Pakaian">Kasual James</a>
+					<a href="#" title="Pakaian">Gaya Kelvin</a>
+				</div>
+			</li>
+			<li>
+				<img src="images/logoRum.png" alt="Rumah">
+				<div class="itemInf">
+					<h2>Rumah</h2>
+					<p>Hunian mewah di kawasan elit<br>daratan Cina</p>
+					<a href="#" title="Rumah">Nippon Style</a>
+					<a href="#" title="Pakaian">Jepit Sungai</a>
+					<a href="#" title="Pakaian">Ultra Tinggi</a>
+				</div>
+			</li>
+			<li>
+				<img src="images/logoPlu.png" alt="Plus-Plus">
+				<div class="itemInf">
+					<h2>Plus-Plus</h2>
+					<p>不用问。<br>只需点击它！</p>
+					<a href="#" title="Plus-Plus">?</a>
+					<a href="#" title="Pakaian">??</a>
+					<a href="#" title="Pakaian">???</a>
+				</div>
+			</li>
+		</ul>
+	</div>
+	</body>
+</html>
---
+++ macro/header.php
@@ -0,0 +1,55 @@
+<?PHP
+  echo "<head>
+    <base href=\"http://localhost/kaskong/\" target=\"_self\">
+    <title>KasKong</title>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/header.css\">
+    <script src=\"script/header_home.js\"></script>
+  </head>
+  <div id=\"headerMenu\">
+		<a href=\"\">
+	    <img id='logo' src=\"images/logo.png\">
+	  </a>  
+	  <li><a href=\"page/asdf.php\">Makanan</a></li>
+	  <li><a href=\"page/asdf.php\">Minuman</a></li>
+	  <li><a href=\"page/asdf.php\">Pakaian</a></li>
+	  <li><a href=\"page/asdf.php\">Rumah</a></li>
+	  <li><a href=\"page/asdf.php\">Plus-Plus</a></li>
+	  <div id='rightBox'>
+	    <a id=\"shopbag\" href=\"#\"><img src=\"images/shopbag.png\"></a></p>
+	    <div id=\"headerControl\">
+        <button onclick=\"toggleLogin()\">Login</button>
+        <button>Register</button>
+        welcome username!
+        <button >Logout</button>
+      </div>
+	  </div>
+  <div id=\"loginPop\">
+    <form id='loginForm' method=\"post\" autocomplete=\"off\">
+      <table>  
+        <tr>  
+         <td>  
+           Username:  
+         </td>  
+         <td>  
+           <input style=\"width: 125px;\" type=\"text\" name=\"user\" />  
+         </td>  
+        </tr>  
+        <tr>  
+         <td>
+           Password:  
+         </td>  
+         <td>  
+           <input style=\"width: 125px;\" type=\"password\" name=\"pass\" />  
+         </td>
+        </tr>  
+        <tr>
+         <td colspan=\"2\" align=\"right\" valign=\"bottom\">  
+           <input type=\"submit\" id=\"subLog\" value=\"Log me in!\"   />
+         </td>  
+        </tr>  
+        </table>
+    </form>
+  </div>
+	  <h3>Barang? Boleh sama... Kualitas? Dijamin <i>Oeh</i> punya!</h3>
+	</div>";
+?>

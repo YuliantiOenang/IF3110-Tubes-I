99fd4e1247cd8a08c626f107d5742ec55f3ba62c
Faiz Ilham <indigo.republica@gmail.com>
2013-10-28 07:04:57 +0700

    mulai bikin page kategori

---
+++ src/category.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html />
+<html>
+<head>
+<title>Barang</title>
+<link rel="stylesheet" href="css/global.css" />
+<link rel="stylesheet" href="css/barang.css" />
+
+<script src="js/ajax.js"></script>
+<script src="js/transaction.js"></script>
+</head>
+<body>
+
+</body>
+</html>

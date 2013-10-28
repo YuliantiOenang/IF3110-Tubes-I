45ddfb679396ec8d60dc72990799191fdd065db1
Iskandar Setiadi <freedom_holicx@yahoo.com>
2013-10-28 18:39:42 +0800

    Merging code

--- src/lihat/user/addcreditcard.php
+++ src/lihat/user/addcreditcard.php
@@ -9,7 +9,7 @@
 <form action="" method="POST">
 	Card Number : <input type="text" name="card_number"><br>
 	Name: <input type="text" name="name"><br>
-	Expired Date : <input type="text" name="expired_date"><br>
+	Expired Date (YYYY-MM-DD) : <input type="text" name="expired_date"><br>
 	<input type="submit" value="submit" name="submit">
 </form>
 </body>

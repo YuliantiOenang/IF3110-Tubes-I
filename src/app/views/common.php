f01c19c494307563caa753b201ea32d2249db770
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2013-10-29 05:56:55 +0700

    Add common template

---
+++ src/app/views/common.php
@@ -0,0 +1,10 @@
+<?php require SITEPATH . '/app/views/head.php' ?>
+
+<body> 
+    <div id="container">
+        <?php require SITEPATH . '/app/views/header.php' ?>
+
+		<h3> <?php echo $text ?></h3>
+	</div>
+</body>
+</html>

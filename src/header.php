dcb1172c5083542f5b246533f2b2efcd9a3ab872
Faiz Ilham <indigo.republica@gmail.com>
2013-10-28 07:04:33 +0700

    ngelayout lagi

--- src/header.php
+++ src/header.php
@@ -1,8 +1,11 @@
-<div class="header">
-	<div class="header-mask">
-	<div class="htitle">RuSerBa Online Shop</div>
+<div class="header-wrapper">
+	<div class="header-float">
+		<a href="#">Login</a>|<a href="#">Signup</a>
+	</div>
+	<div class="header">
+		<div class="htitle">Ruko Serba Ada</div>
+		<div class="menu">
+		<a href="#" class="item">Makanan</a><a href="#" class="item">Peralatan Sekolah</a><a href="#" class="item">Peralatan Rumah</a><a href="#" class="item">Peralatan Serbaguna</a><a href="#" class="item">Mainan</a>
+		</div>	
 	</div>
 </div>
-<div class="menu">
-	<a href="#" class="item">Makanan</a><a href="#" class="item">Peralatan Sekolah</a><a href="#" class="item">Peralatan Rumah</a><a href="#" class="item">Peralatan Serbaguna</a><a href="#" class="item">Mainan</a>
-</div>	

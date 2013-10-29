543d5f6b6191ff9c6c972ea4dbbe395b8cf9875c
aiothealchemist <antelop.boy@gmail.com>
2013-10-29 22:01:32 +0700

    betulin register

--- src/register.php
+++ src/register.php
@@ -1,22 +1,25 @@
 <?php
-	// // Contoh dari file content.
+	// Contoh dari file content.
 
-	// // Selalu include ini di awal.
-	// require_once('ref.php');
+	// Selalu include ini di awal.
+	require_once('ref.php');
 
-	// // Tentukan file css dan javascript.
-	// $css_file = 'styles/register.css';
-	// $js_file = 'scripts/register.js';
+	// Include auth.php, untuk memeriksa user sudah login atau belum.
+	require_once('auth.php');
 
-	// // Tulis title
-	// $page_title = 'Register into RuSerBa!';
+	// Tentukan file css dan javascript.
+	$css_file = 'styles/register.css';
+	$js_file = 'scripts/register.js';
 
-	// // Include begin.
-	// require_once('begin.php');
+	// Tulis title
+	$page_title = 'Register into RuSerBa!';
+
+	// Include begin.
+	require_once('begin.php');
 ?>
-<link link rel="stylesheet" type="text/css" href='styles/register.css'>
 <p>
 Silakan registrasi. bagian yang berlabel bintang (*) wajib diisi.
+<button id="button_right">Register Me!</button>
 </p>
 <div id="register_form">
 	<label>Username *</label>
@@ -39,23 +42,22 @@ Silakan registrasi. bagian yang berlabel bintang (*) wajib diisi.
 	<div id=responseFullname> </div>
 	<br>
 	<label>Provinsi</label>
-	<input type="text" name="provinsi">
+	<input type="text" id="provinsi">
 	<br>
 	<label>Kota/Kabupaten</label>
-	<input type="text" name="kota">
+	<input type="text" id="kota">
 	<br>
 	<label>Alamat</label>
-	<input type="text" name="alamat">
+	<input type="text" id="alamat">
 	<br>
 	<label>Kode Pos</label>
-	<input type="text" name="kodepos" onblur="cekAngka()">
+	<input type="text" id="kodepos" onblur="cekAngka(document.getElementById('kodepos').value)">
 	<div id=responseAngka> </div>
 	<br>
 	<label>Telepon</label>
-	<input type="text" name="telepon" onblur="cekAngka()">
+	<input type="text" id="telepon" onblur="cekAngka(document.getElementById('telepon').value)">
 	<div id=responseAngka> </div>
 </div>
-<button id="button_right">Register Me!</button>
 <?php
 	require_once('end.php');
 ?>
--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,5 +1,45 @@
 function cekUsername(){
 	var username = getElementById("username").value;
-	
+	var text = "";
+	if (username.length > 5){
+		if (){
+			text = "";
+		}
+	}
+	getElementById("responseUsername").innerHTML = text;
+}
+
+function cekEmail(){
+	var email = getElementById("email").value.split('@');
+	var text = "";
+	if (email.length == 2 && email[0].length > 0){
+		var email1 = email.split('.');
+		if (email1.length > 1 && email1[0].length > 0
+				&& email1[email1.length-1].length > 1){
+			text = "";
+		}
+	}
+	getElementById("responseEmail").innerHTML = text;
+}
+
+function cekPasswordMatch(pass, passC){
+	var text = "";
+	if (pass.localeCompare(passC) != 0){
+		text = "";
+	}
+	getElementById("responsePassword").innerHTML = text;
+}
+
+function cekFullname(){
+	var username = getElementById("fullname").value;
+
 	if (x != null)
+}
+
+function cekAngka(angka){
+	if (!isNaN(parseFloat(angka)) && isFinite(angka)){
+		
+	} else {
+		
+	}
 }

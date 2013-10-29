4041c400bb55274e87a8ab1ef38aa6f907f9079d
Ichlasul Amal <13511075@std.stei.itb.ac.id>
2013-10-29 07:28:20 +0700

    Change error redirect, filter & session

--- src/app/controllers/register.php
+++ src/app/controllers/register.php
@@ -33,22 +33,27 @@ Class RegisterController Extends BaseController {
 	 * Selanjutnya masukkan ke database
 	 */
 	public function new_customer() {
+		/*** begin our session ***/
+		session_start();
+
 		//validasi sedikit
-		if (empty($_POST["username"]) or empty($_POST["email"]) or empty($_POST["password"]) or empty($_POST["fullname"])) {
-			header("Location: " . SITEURL); die(); //redirect
+		if (!isset($_POST["username"], $_POST["email"], $_POST["password"], $_POST["fullname"])
+			or $_POST['form_token'] != $_SESSION['form_token'])  {
+			header("Location: " . SITEURL . "/register/failed" ); die();
 		} else {
-			$customer['username'] = $_POST["username"];
-			$customer['email'] = $_POST["email"];
-			$customer['password'] = hash('sha256', ($_POST["password"]));
-			$customer['fullname'] = $_POST["fullname"];
-			$customer['phone'] = $_POST["phone"];
-			$customer['address'] = $_POST["address"];
-			$customer['city'] = $_POST["city"];
-			$customer['province'] = $_POST["province"];
-			$customer['postcode'] = $_POST["postcode"];
+			$customer['username'] =  filter_var($_POST["username"], FILTER_SANITIZE_STRING);
+			$customer['email'] =  filter_var($_POST["email"], FILTER_SANITIZE_STRING);
+			$customer['password'] = hash('sha256', filter_var($_POST["password"], FILTER_SANITIZE_STRING));
+			$customer['fullname'] =  filter_var($_POST["fullname"], FILTER_SANITIZE_STRING);
+			$customer['phone'] =  filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
+			$customer['address'] =  filter_var($_POST["address"], FILTER_SANITIZE_STRING);
+			$customer['city'] =  filter_var($_POST["city"], FILTER_SANITIZE_STRING);
+			$customer['province'] =  filter_var($_POST["province"], FILTER_SANITIZE_STRING);
+			$customer['postcode'] =  filter_var($_POST["postcode"], FILTER_SANITIZE_STRING);
 	
 			if (Customer::addCustomer($this->registry, $customer)) {
 				//redirect
+				$_SESSION['logged_username'] = $customer['username'];
 				header("Location: " . SITEURL . "/register/card/"); die();
 				//http_redirect (SITEURL . '/register/card', array ('username' => $customer['username']), true, HTTP_REDIRECT_POST );
 			} else {
@@ -63,13 +68,16 @@ Class RegisterController Extends BaseController {
 	 */
 
 	public function new_creditcard() {
+		session_start();
 		//validasi sedikit
-		if (empty($_POST["card_name"]) or empty($_POST["card_number"]) or empty($_POST["card_expired"])) {
-			header("Location: " . SITEURL); die();
+		if (!isset($_POST["card_name"], $_POST["card_number"], $_POST["card_expired"])
+			or $_POST['form_token'] != $_SESSION['form_token']) {
+			header("Location: " . SITEURL . "/register/failed" ); die();
 		} else {
-			$customer['card_name'] = $_POST["card_name"];
-			$customer['card_number'] = $_POST["card_number"];
-			$customer['card_expired'] = $_POST["card_expired"];
+			$customer['username'] = $_SESSION['logged_username'];
+			$customer['card_name'] = filter_var($_POST["card_name"], FILTER_SANITIZE_STRING);
+			$customer['card_number'] = filter_var($_POST["card_number"], FILTER_SANITIZE_STRING);
+			$customer['card_expired'] = filter_var($_POST["card_expired"], FILTER_SANITIZE_STRING);
 	
 			if (Customer::updateCreditCard($this->registry, $customer)) {
 				//redirect

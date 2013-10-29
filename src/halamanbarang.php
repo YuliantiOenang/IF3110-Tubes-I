3fc2b9ebea371b43e7dfc25ed0fa82bd96bff299
Fakhri <fakhri.abdr@gmail.com>
2013-10-29 18:35:44 +0700

    gambar dll

---
+++ src/halamanbarang.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+	<title>Regristrasi Identitas</title>
+	<link href="css/home.css" rel="stylesheet" type="text/css"/>	
+</head>
+
+<body>
+	<div class="kategori">
+		Kategori A
+	</div>
+	<div class="container">
+		<div class="productdetailright">
+			<div class="boxproduct">
+				Gambar product 180x180
+			</div>
+			<div class="deskripsi_barang">
+				Nama Barang
+			</div>
+			<div class="field_kiri">
+				Harga : Rp0,00
+			</div>
+			<div>
+				<div class="field_kiri">
+					Total
+				</div>
+				<button class="mybutton" onclick="">Beli</button>
+			</div>
+		</div>
+		
+		<div class="productdetailleft">
+			<div class="boxproduct">
+				Gambar product 180x180
+			</div>
+			<div class="deskripsi_barang">
+				Nama Barang
+			</div>
+			<div class="field_kiri">
+				Harga : Rp0,00
+			</div>
+			<div>
+				<div class="field_kiri">
+					Total
+				</div>
+				<script>
+					message = "Clicked Button";
+					document.write('<button class="mybutton" onclick="">Beli</button>');
+				</script> 
+			</div>
+		</div>
+				
+		</div>
+	</div>
+</body>
+</html>

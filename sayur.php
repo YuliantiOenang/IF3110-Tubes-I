663e4de265d0e78f2c256df8de315de948865e46
dinulfa <dinulfa@gmail.com>
2013-10-29 19:55:58 +0700

    sayur

---
+++ sayur.php
@@ -0,0 +1,59 @@
+		<article class="lifted_content_box">
+		<h1>Sayur-mayur</h1>
+		<table border="0" width="200" align="center">
+		<tr>
+			<td>
+				<a href="index.php?page=barang&nama=Bayam">
+				<img src="images/Sayur/bayam.png" alt="bayam segar" width="200"/></a>
+			</td>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/brokoli.png" alt="brokoli segar" width="200"></a>
+			</td>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/kentang.png" alt="kentang segar" width="200"/></a>
+			</td>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/kangkung.png" alt="kangkung segar" width="200"/></a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/Kubis.png" alt="kubis segar" width="200"/></a>
+			</td>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/paprika hijau.png" alt="paprika hijau segar" width="200"/></a>
+			</td>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/Paprika kuning.png" alt="paprika kuning segar" width="200"/></a>
+			</td>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/paprika merah.png" alt="paprika merah segar" width="200"/></a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/sawi hijau.png" alt="sawi hijau segar" width="200"/></a>
+			</td>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/sawi putih.png" alt="sawi putih segar" width="200"/></a>
+			</td>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/terong ungu.png" alt="terong ungu segar" width="200"/></a>
+			</td>
+			<td>
+				<a href="home.php" target="_blank">
+				<img src="images/Sayur/tomat.png" alt="tomat segar" width="200"/></a>
+			</td>
+		</tr>		
+		</table>
+		</article>

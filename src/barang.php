349d02a8a296891cc09a9a08d72e148165bc11cc
Dimas <dimas11@rocketmail.com>
2013-10-29 19:53:02 +0700

    Pagination setiap kategori barang
    1 page 10 barang

--- src/barang.php
+++ src/barang.php
@@ -1,62 +1,143 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Registrasi Profil</title>
-        <link rel="stylesheet" href="style.css" type="text/css"/>
-        
-                
-       </head>
-    <body>
-        
- <?php
- 		$con=mysqli_connect("127.0.0.1","root","","ruserba");
-		if (mysqli_connect_errno($con))
-        {
-            echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        }
- 		$query.= "SELECT * FROM barang";
-       	$result = mysql_query($query);
- 		$rows = array();
- 		if($result)
- 		{
-      		while($row=mysql_fetch_assoc($result)){
-           	$rows[] = $row;
-      		}
- 		}
+<?php
+	session_start();
+	ob_start();
+	$link = mysql_connect("localhost","root","") or die("Can't connect to database. Contact Your Administrator.");	
+	mysql_select_db("ruserba") or die("Cannot select DB. Contact your web administrator.");
+?>
+<html>
+	<head>
+		<title>RuSerBa</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
 		
+	</head>
+	<body>
+		<?php
+			include 'header.php';
+		?>
 		
- ?>
-        
-           	<table height= "600" width="800">
-			<tr style="vertical-align: top; text-align:top display:inline-block">
-			<thead>   
-      			<tr>
-      				<td>Nama</td>
-      				<td> Harga</td>
-      				<td>Kategori</td>
-      				<td>Stok</td>
-      				<td>foto</td>
-        
-     			 </tr>
-  			</thead> 
-  <?php foreach ($rows as $row){?>
-  <tr>
-      <td><?php echo $row['nama_barang']?></td>
-      <td><?php echo $row['harga']?></td>
-      <td><?php echo $row['kategori']?></td>  
-      <td><?php echo $row['stok']?></td>  
-      <td><?php echo $row['url_gambar']?></td>  
-      
-      
-  </tr>
-  <?php }?>
-</tr> 
-            <footer>
-            <br>
-            About us
-            </footer>    
-    </body>
+		<div id="content_body">
+			<h3 ><a href="makanan.php">Makanan</a> </h3>
+			<div class="content_item">
+				<ul class="horizontal_list">
+				<?php
+					$query = "SELECT * FROM barang WHERE kategori=0 ORDER BY jumlah_jual ";
+					$result = mysql_query($query, $link);
+					if(mysql_num_rows($result)>0) {
+						while($row = mysql_fetch_array($result)) {
+							echo '<li>';
+							echo '<div class="barang">';
+							echo '<a href="detail.php?id='.$row['id_barang'].'">';
+							$array = explode(',',$row['url_gambar']);
+							echo '<img src="'.$array[0].'" />';
+							echo '<h4>'.$row['nama_barang'].'</h4>';
+							echo '<div>'.$row['harga'].'</div>';
+							echo '</a>';
+							echo '</div>';
+							echo '</li>';
+						}
+					}
+				?>
+				</ul>
+			</div>
+			<h3 ><a href="minuman.php">Minuman</a> </h3>
+			<div class="content_item">
+				<ul class="horizontal_list">
+					<?php
+					$query = "SELECT * FROM barang WHERE kategori=1 ORDER BY jumlah_jual ";
+					$result = mysql_query($query, $link);
+					if(mysql_num_rows($result)>0) {
+						while($row = mysql_fetch_array($result)) {
+							echo '<li>';
+							echo '<div class="barang">';
+							echo '<a href="detail.php?id='.$row['id_barang'].'">';
+							$array = explode(',',$row['url_gambar']);
+							echo '<img src="'.$array[0].'" />';
+							echo '<h4>'.$row['nama_barang'].'</h4>';
+							echo '<div>'.$row['harga'].'</div>';
+							echo '</a>';
+							echo '</div>';
+							echo '</li>';
+						}
+					}
+				?>
+				</ul>
+			</div>
+			<h3 ><a href="alattulis.php">Alat Tulis</a> </h3>
+			<div class="content_item">
+				<ul class="horizontal_list">
+					<?php
+					$query = "SELECT * FROM barang WHERE kategori=2 ORDER BY jumlah_jual ";
+					$result = mysql_query($query, $link);
+					if(mysql_num_rows($result)>0) {
+						while($row = mysql_fetch_array($result)) {
+							echo '<li>';
+							echo '<div class="barang">';
+							echo '<a href="detail.php?id='.$row['id_barang'].'">';
+							$array = explode(',',$row['url_gambar']);
+							echo '<img src="'.$array[0].'" />';
+							echo '<h4>'.$row['nama_barang'].'</h4>';
+							echo '<div>'.$row['harga'].'</div>';
+							echo '</a>';
+							echo '</div>';
+							echo '</li>';
+						}
+					}
+				?>
+				</ul>
+			</div>
+			<h3 ><a href="kebersihan.php">Kebersihan</a> </h3>
+			<div class="content_item">
+				<ul class="horizontal_list">
+					<?php
+					$query = "SELECT * FROM barang WHERE kategori=3 ORDER BY jumlah_jual ";
+					$result = mysql_query($query, $link);
+					if(mysql_num_rows($result)>0) {
+						while($row = mysql_fetch_array($result)) {
+							echo '<li>';
+							echo '<div class="barang">';
+							echo '<a href="detail.php?id='.$row['id_barang'].'">';
+							$array = explode(',',$row['url_gambar']);
+							echo '<img src="'.$array[0].'" />';
+							echo '<h4>'.$row['nama_barang'].'</h4>';
+							echo '<div>'.$row['harga'].'</div>';
+							echo '</a>';
+							echo '</div>';
+							echo '</li>';
+						}
+					}
+				?>
+				</ul>
+			</div>
+			<h3 ><a href="obat.php">Obat</a> </h3>
+			<div class="content_item">
+				<ul class="horizontal_list">
+					<?php
+					$query = "SELECT * FROM barang WHERE kategori=4 ORDER BY jumlah_jual ";
+					$result = mysql_query($query, $link);
+					if(mysql_num_rows($result)>0) {
+						while($row = mysql_fetch_array($result)) {
+							echo '<li>';
+							echo '<div class="barang">';
+							echo '<a href="detail.php?id='.$row['id_barang'].'">';
+							$array = explode(',',$row['url_gambar']);
+							echo '<img src="'.$array[0].'" />';
+							echo '<h4>'.$row['nama_barang'].'</h4>';
+							echo '<div>'.$row['harga'].'</div>';
+							echo '</a>';
+							echo '</div>';
+							echo '</li>';
+						}
+					}
+				?>
+				</ul>
+			</div>
+			<h3>Mekanisme Belanja</h3>
+			<div class="content_item">
+			<p>Lorem Insum Dolor Sit Amer.Lorem Insum Dolor Sit Amer.Lorem Insum Dolor Sit Amer.Lorem Insum Dolor Sit Amer.
+			Lorem Insum Dolor Sit Amer.Lorem Insum Dolor Sit Amer.Lorem Insum Dolor Sit Amer.
+			Lorem Insum Dolor Sit Amer.Lorem Insum Dolor Sit Amer.
+			Lorem Insum Dolor Sit Amer.Lorem Insum Dolor Sit Amer.<p>
+			</div>
+		</div>
+	</body>
 </html>

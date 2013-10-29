34379cee22042a3a8fc99dc915e3844a32b3bc41
harits <adilelfahmi@gmail.com>
2013-10-29 14:31:36 +0700

    apapun

--- pages/list.php
+++ pages/list.php
@@ -34,18 +34,24 @@
 					elseif($name == "household")
 						return "Household Essentials";
 				}
+				
+				//SEARCH BAR
 				?>
 				<form action="search.php" method="get">
-					Nama: <input type="text" name="query_name" size="30" onkeyup="showResult(this.value)">
-					<div id="livesearch"></div>
-					Harga: <input type="text" name="query_price" size="30"><br/>
-					Kategori: <select name="query_category">
-					  <option value="baking">Baking</option>
-					  <option value="beverages">Beverages</option>
-					  <option value="cansoups">Canned Goods & Soups</option>
-					  <option value="household">Household Essentials</option>
-					</select>
-					<input type="submit" value="Submit">
+					<div class='sbox'>
+						<h3 class='sb_name'>Pencarian</h3>
+						<div class='sb_name'>Nama:</div><div class='sb_value'><input type="text" name="query_name" size="30" onkeyup="showResult(this.value)"></div>
+						<div id="livesearch"></div>
+						<div class='sb_name'>Harga:</div><div class='sb_value'><input type="text" name="query_price" size="30"></div>
+						<div class='sb_name'>Kategori:</div><div class='sb_value'><select name="query_category">
+						  <option value="baking">Baking</option>
+						  <option value="beverages">Beverages</option>
+						  <option value="fresh">Fresh Food</option>
+						  <option value="cansoups">Canned Goods & Soups</option>
+						  <option value="household">Household Essentials</option>
+						</select></div>
+						<input type="submit" value="Submit">
+					</div>
 				</form>
 				
 				<?php

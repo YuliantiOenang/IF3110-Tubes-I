0e670496a2ef35fca0203966231e569482970866
Muhammad Ikhsan <13511064@std.stei.itb.ac.id>
2013-10-29 15:30:43 +0700

    Bagian registrasi kartu kredit sudah pakai AJAX

--- src/registrasikredit.php
+++ src/registrasikredit.php
@@ -13,33 +13,15 @@ jika gagal memberikan notifikasi gagal dan kembali ke halaman registrasi kartu k
 <script src="validation.js"></script>
 <h1> REGISTRASI KARTU KREDIT </h1>
 <p> Registrasi kartu kredit dilakukan melalui form yang ada dibawah ini </p>
-<form method="post">
+<form method="post" action="submit.php">
 	<label for "cardnumber">Card Number : </label>
 	<input type="text" id="cardnumber" name="cardnumber" "></br>
 	<label for "namecard">Name on card : </label>
 	<input type="text" id="namecard" name="namecard" /><br/>
-	<label for "expireddate">Expired Date : </label>
+	<label for "expireddate">Expired Date (Format MM/YY) : </label>
 	<input type="text" id="expireddate" name="expireddate" /><br/>
-	<button type="button" onclick="validation(this.form)"  >Submit</button>
-	<?php
-		
-/* <!--		if(isset($_POST["ajax"])){
-			$response = handleTransactionAjax();
-			if ($response != null){
-				exit($response);
-			}
-			exit();	
-		} else if(!isset($_GET["id"])){
-		header("Location: indeks.php");
-		} -->
- 		$card_number = $_POST['cardnumber'];
-		$name_card = $_POST['namecard'];
-		$expired_date = $_POST ['expireddate'];
- 		echo 'Thanks for submitting the form. <br/>';
-		echo 'Nomor kartu : ' . $card_number . '<br/>' ;
-		echo 'Nama pada kartu : ' . $name_card. '<br/>';
-		echo 'Tanggal Kartu Expired : ' . $expired_date . '<br/>';
- */	?>
+	<!--<button type="button" onclick="validation(this.form)"  >Submit</button>-->
+	<input type="button" value = "Submit" onclick="validation(this.form)" />
 </body>
 </form>
 </html>

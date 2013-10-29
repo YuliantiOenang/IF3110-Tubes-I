2ca80e5c37d5e6e0e0a5b7bf8d5c780acbe3438a
Iskandar Setiadi <freedom_holicx@yahoo.com>
2013-10-29 18:07:28 +0800

    Stashing previous commit

--- src/lihat/barang/beli.php
+++ src/lihat/barang/beli.php
@@ -3,71 +3,64 @@
 <head>
 <meta charset="UTF-8">
 	<title> Beli Barang </title>
+    <link href="<?=SITE_ROOT.NAME_ROOT;?>/css/table.css" rel="stylesheet"/>
+	<link href="<?=SITE_ROOT.NAME_ROOT;?>/css/profile.css" rel="stylesheet"/>
     <link href="<?=SITE_ROOT.NAME_ROOT;?>/css/mainstyle.css" rel="stylesheet"/>
 </head>
 <body>
-	<div id="header">
-		<div id="toplogo">
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/home"><img id="logo" src="<?=SITE_ROOT.NAME_ROOT;?>/gambar_barang/logoruserba.jpg" alt="home"></a>
+<h2>Pembelian Barang</h2>
+
+		<div id="table">
+		<div class="header">
+			<span class="kolom satu" id="narrowcolumn">No</span>
+			<span class="kolom dua">Nama Barang</span>
+			<span class="kolom tiga">Quantity</span>
+            <span class="kolom empat">Harga Total</span>
+			<span class="kolom lima">Deskripsi Tambahan</span>
+			<span class="kolom enam">Status</span>
+
 		</div>
-		<div id="logintop">
+		<?php
+		$i=0; $total_harga=0;
+		while ($row = mysql_fetch_object($data['listBarang']))
+		{
+			$i++;
+            if ($row->status == 0) 
+            {
+              $total_harga = $total_harga + $row->jumlah_barang * $row->harga_barang;
+		?>
+		<div class="baris">
+			<span class="kolom satu" id="narrowcolumn"><?=$i;?></span>
+			<span class="kolom dua"><?=$row->nama_barang;?></span>
+			<span class="kolom tiga"><?=$row->jumlah_barang;?></span>
+            <span class="kolom empat"><?=$row->jumlah_barang * $row->harga_barang;?></span>
+			<span class="kolom lima"><?=$row->deskripsi_tambahan;?></span>
+			
 			<?php
-				if ($_SESSION['username'] == null)
-				{
-			?>
-			<a href="#login_popup"><strong>Login</strong></a>
-			<br><br>
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user/register"><strong>Register</strong></a>
-			<?php
-				} else {
-			?>
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user/logout"><strong>Logout</strong></a>
-			<br><br>
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user"><strong>Profile</strong></a>
-			<?php
-				}
-			?>
-			<form action="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari" method="GET">
-			<p id ="search"><b>Cari Barang:</b>
-			Nama : <input type="text" name="search">
-			Kategori : 
-			<select name="kategori">
-			<option value="">--Pilih--</option>
-			<?php
-			while ($row = mysql_fetch_object($data['listCateg']))
+			if ($row->status == 0)
 			{
 			?>
-			<option value="<?=$row->name;?>"><?=$row->name;?></option>
+				<span class="kolom enam"><font color="red">Barang belum dibayar / dibeli</font></span>
+			<?php
+			}
+			else
+			{
+			?>
+				<span class="kolom enam"><font color="green">Barang sudah dibayar / dibeli</font></span>
 			<?php
 			}
 			?>
-			</select>
-			Harga : <input type="text" name="harga">
-			<input type="submit"><br>
-			<span id="radiobutt">
-			<input type="radio" name="operator" value="L" checked>Less than
-			<input type="radio" name="operator" value="E">Equal to
-			<input type="radio" name="operator" value="G">Greater than
 			</span>
-			</p>
-			</form>
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user/cart"><img id="tasbelanja" src="<?=SITE_ROOT.NAME_ROOT;?>/gambar_barang/tasbelanja.jpg" alt="Tas Belanja"></a>	
 		</div>
-		<div id="kategori">
-			 <p><span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Sembako"><strong>Sembako</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Handphone"><strong>Handphone</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Peralatan+Listrik"><strong>PeralatanElektronik</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Aksesoris+Komputer"><strong>AksesorisKomputer</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Perabotan+Rumah"><strong>PerabotanRumah</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Alat+Tulis"><strong>AlatTulis</strong></a></span>
-			 <p>
-		</div>
-	</div>
-<h2>Pembelian Barang</h2>
+		<?php
+            }
+		}
+		?>
+		</div><br>
+        
+    Total Harga (Yang belum dibeli) : <?=$total_harga;?><br><br>
+
 <form action="" method="POST">
-Nama Barang : <?=$data['nama_barang'];?><br>
-Stok	: <?=$data['stok'];?><br>
-Jumlah Pembelian : <input type="text" name="qty"><br>
 Kartu Kredit :
 <select name="kartu">
 <?php
@@ -79,8 +72,6 @@ while ($row = mysql_fetch_object($data['listCC']))
 }
 ?>
 </select><br>
-Deskripsi Tambahan : <textarea name="deskripsi_tambahan" id="deskripsi_tambahan"></textarea><br>
-<input type="hidden" name="id_barang" value="<?=$data['id_barang'];?>"><br>
 <input type="submit" value="submit" name="submit">
 </form>
 </body>
--- src/lihat/barang/search.php
+++ src/lihat/barang/search.php
@@ -4,120 +4,62 @@
 <meta charset="UTF-8">
 	<title> Search </title>
 	<link href="<?=SITE_ROOT.NAME_ROOT;?>/css/table.css" rel="stylesheet"/>
-    <link href="<?=SITE_ROOT.NAME_ROOT;?>/css/mainstyle.css" rel="stylesheet"/>
 </head>
 <body>
-	<div id="header">
-		<div id="toplogo">
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/home"><img id="logo" src="<?=SITE_ROOT.NAME_ROOT;?>/gambar_barang/logoruserba.jpg" alt="home"></a>
-		</div>
-		<div id="logintop">
-			<?php
-				if ($_SESSION['username'] == null)
-				{
-			?>
-			<a href="#login_popup"><strong>Login</strong></a>
-			<br><br>
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user/register"><strong>Register</strong></a>
-			<?php
-				} else {
-			?>
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user/logout"><strong>Logout</strong></a>
-			<br><br>
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user"><strong>Profile</strong></a>
-			<?php
-				}
-			?>
-			<form action="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari" method="GET">
-			<p id ="search"><b>Cari Barang:</b>
-			Nama : <input type="text" name="search">
-			Kategori : 
-			<select name="kategori">
-			<option value="">--Pilih--</option>
-			<?php
-			while ($row = mysql_fetch_object($data['listCateg']))
-			{
-			?>
-			<option value="<?=$row->name;?>"><?=$row->name;?></option>
-			<?php
-			}
-			?>
-			</select>
-			Harga : <input type="text" name="harga">
-			<input type="submit"><br>
-			<span id="radiobutt">
-			<input type="radio" name="operator" value="L" checked>Less than
-			<input type="radio" name="operator" value="E">Equal to
-			<input type="radio" name="operator" value="G">Greater than
+
+<h3> Hasil Pencarian untuk keyword <font color="green"><?=$data['nama_barang'];?></font> </h3>
+
+<div id="table">
+<div class="header">
+	<span class="kolom satu">Nama Barang</span>
+	<span class="kolom dua">Gambar Barang</span>
+	<span class="kolom tiga">Harga</span>
+	<span class="kolom empat">Stok</span>
+	<span class="kolom lima">Aksi</span>
+</div>
+<?php
+while($row = mysql_fetch_object($data['barang']))
+{
+?>
+	<div class="baris">
+		<span class="kolom satu"><?=$row->nama_barang;?></span>
+		<span class="kolom dua"><img src="<?=SITE_ROOT.NAME_ROOT;?>/gambar_barang/<?=$row->gambar;?>" height=100px width=100px></span>
+		<span class="kolom tiga"><?=$row->harga_barang;?></span>
+		<?php
+		if ($row->jumlah_barang > 0)
+		{
+		?>
+		<span class="kolom empat"><?=$row->jumlah_barang;?></span>
+		<span class="kolom lima"><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/beli?id=<?=$row->id;?>">Beli</a>
+		    <a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/detail?id=<?=$row->id;?>">Detail</a>
+		</span>
+		<?php
+		}
+		else
+		{
+		?>
+			<span class="kolom empat">Habis</span>
+			<span class="kolom lima">
+			  <a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/detail?id=<?=$row->id;?>">Detail</a>
 			</span>
-			</p>
-			</form>
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user/cart"><img id="tasbelanja" src="<?=SITE_ROOT.NAME_ROOT;?>/gambar_barang/tasbelanja.jpg" alt="Tas Belanja"></a>	
-		</div>
-		<div id="kategori">
-			 <p><span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Sembako"><strong>Sembako</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Handphone"><strong>Handphone</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Peralatan+Listrik"><strong>PeralatanElektronik</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Aksesoris+Komputer"><strong>AksesorisKomputer</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Perabotan+Rumah"><strong>PerabotanRumah</strong></a></span>
-				<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Alat+Tulis"><strong>AlatTulis</strong></a></span>
-			 <p>
-		</div>
+		<?php		
+		}
+		?>
 	</div>
-	<div class="basiccontent" id="giantcontent">
-		<h3> Hasil Pencarian untuk keyword <font color="green"><?=$data['nama_barang'];?></font> </h3>
-		
-		<div id="table">
-		<div class="header">
-			<span class="kolom satu">Nama Barang</span>
-			<span class="kolom dua">Gambar Barang</span>
-			<span class="kolom tiga">Harga</span>
-			<span class="kolom empat">Stok</span>
-			<span class="kolom lima">Aksi</span>
-		</div>
-		<?php
-		while($row = mysql_fetch_object($data['barang']))
-		{
-		?>
-			<div class="baris">
-				<span class="kolom satu"><?=$row->nama_barang;?></span>
-				<span class="kolom dua"><img src="<?=SITE_ROOT.NAME_ROOT;?>/gambar_barang/<?=$row->gambar;?>" height=100px width=100px></span>
-				<span class="kolom tiga"><?=$row->harga_barang;?></span>
-				<?php
-				if ($row->jumlah_barang > 0)
-				{
-				?>
-				<span class="kolom empat"><?=$row->jumlah_barang;?></span>
-				<span class="kolom lima"><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/beli?id=<?=$row->id;?>">Beli</a>
-					<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/detail?id=<?=$row->id;?>">Detail</a>
-				</span>
-				<?php
-				}
-				else
-				{
-				?>
-					<span class="kolom empat">Habis</span>
-					<span class="kolom lima">
-					<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/detail?id=<?=$row->id;?>">Detail</a>
-					</span>
-				<?php		
-				}
-				?>
-			</div>
-		<?php
-		}
-		?>
-		</div>
-		
-		Halaman : 
-		<?php
-		for ($i=0; $i<$data['jmlPage']; $i++)
-		{
-		?>
-			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?page=<?=$i;?>&search=<?=$data['nama_barang']?>&kategori=<?=$data['kategori'];?>&harga=<?=$data['harga'];?>&operator=<?=$data['operator'];?>"> <?=$i;?> </a>
-		<?php
-		}
-		?>
-	</div>	
+<?php
+}
+?>
+</div>
+
+Halaman : 
+<?php
+for ($i=0; $i<$data['jmlPage']; $i++)
+{
+?>
+	 <a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?page=<?=$i;?>&search=<?=$data['nama_barang']?>&kategori=<?=$data['kategori'];?>&harga=<?=$data['harga'];?>&operator=<?=$data['operator'];?>"> <?=$i;?> </a>
+<?php
+}
+?>
+
 </body>
 </html>

7e6d72489476ff90a5cf8ce2ea472ed7cc1a916f
Muhamad Ihsan <m.ihsanity@gmail.com>
2013-10-29 20:09:35 +0700

    samain semua header + fix link

--- src/lihat/barang/beli.php
+++ src/lihat/barang/beli.php
@@ -8,6 +8,68 @@
     <link href="<?=SITE_ROOT.NAME_ROOT;?>/css/mainstyle.css" rel="stylesheet"/>
 </head>
 <body>
+	<div id="header">
+		<div id="toplogo">
+			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/home"><img id="logo" src="<?=SITE_ROOT.NAME_ROOT;?>/gambar_barang/logoruserba.jpg" alt="home"></a>
+		</div>
+		<div id="logintop">
+			<?php
+				if ($_SESSION['username'] == null)
+				{
+			?>
+			<a href="#login_popup"><strong>Login</strong></a>
+			<br><br>
+			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user/register"><strong>Register</strong></a>
+			<?php
+				} else {
+			?>
+			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user/logout"><strong>Logout</strong></a>
+			<br><br>
+			<a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user"><strong>Profile</strong></a>
+            <br><br>
+			<p id="welcometext">Welcome, <?php echo $_SESSION['username'] ?></p>
+            <a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/user/cart"><img id="tasbelanja" src="<?=SITE_ROOT.NAME_ROOT;?>/gambar_barang/tasbelanja.jpg" alt="Tas Belanja"></a>        
+
+			<?php
+				}
+			?>
+                        <form action="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari" method="GET">
+                        <p id ="search"><b>Cari Barang:</b>
+                        Nama : <input type="text" name="search">
+                        Kategori : 
+                        <select name="kategori">
+                        <option value="">--Pilih--</option>
+                        <?php
+                        while ($row = mysql_fetch_object($data['listCateg']))
+                        {
+                        ?>
+                        <option value="<?=$row->name;?>"><?=$row->name;?></option>
+                        <?php
+                        }
+                        ?>
+                        </select>
+                        Harga : <input type="text" name="harga">
+                        <input type="submit"><br>
+                        <span id="radiobutt">
+                        <input type="radio" name="operator" value="L" checked>Less than
+                        <input type="radio" name="operator" value="E">Equal to
+                        <input type="radio" name="operator" value="G">Greater than
+                        </span>
+                        </p>
+                        </form>
+        </div>
+                <div id="kategori">
+                         <p>
+								<span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Sembako"><strong>Sembako</strong></a></span>
+                                <span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Handphone"><strong>Handphone</strong></a></span>
+                                <span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Peralatan+Listrik"><strong>PeralatanElektronik</strong></a></span>
+                                <span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Aksesoris+Komputer"><strong>AksesorisKomputer</strong></a></span>
+                                <span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Perabotan+Rumah"><strong>PerabotanRumah</strong></a></span>
+                                <span><a href="<?=SITE_ROOT.NAME_ROOT;?>/index.php/barang/cari?search=&kategori=Alat+Tulis"><strong>AlatTulis</strong></a></span>
+                         </p>
+                </div>
+    </div>
+	<div class="basiccontent" id="giantcontent">
 <h2>Pembelian Barang</h2>
 
 		<div id="table">
@@ -75,5 +137,6 @@ while ($row = mysql_fetch_object($data['listCC']))
 <input type="submit" value="submit" name="submit" <?php if ($total_harga == 0) echo "disabled"; ?>>
 <input type="button" value="back" onClick="history.go(-1);return reset();">
 </form>
+</div>
 </body>
 </html>
